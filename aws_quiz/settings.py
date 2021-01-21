a1ccd78c7a0e3adad7e85efa2a32796b0bcc9ff7
theognis1002 <theognis1002@gmail.com>
2021-01-20 22:22:43 -0500

    updated config vars

--- aws_quiz/settings.py
+++ aws_quiz/settings.py
@@ -16,7 +16,7 @@ sys.path.insert(0, os.path.join(BASE_DIR, "apps"))
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = ["radiant-caverns-84612.herokuapp.com", "localhost", "127.0.0.1"]
 
