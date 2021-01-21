7b783dd5a464cec458d9410ec64ada71412cb1fc
theognis1002 <theognis1002@gmail.com>
2021-01-20 22:12:58 -0500

    updated allowed hosts

--- aws_quiz/settings.py
+++ aws_quiz/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["radiant-caverns-84612.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
