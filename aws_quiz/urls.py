adf9af84abaf6317d9233354fdcb8038813583c7
theognis1002 <theognis1002@gmail.com>
2022-09-12 00:50:29 -0400

    Revert admin page url

--- aws_quiz/urls.py
+++ aws_quiz/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("staff/", admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", include("content.urls")),
     path("", include("users.urls")),
 ]
